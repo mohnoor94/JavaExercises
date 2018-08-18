348ca05b14ff88cb9ad358f9330d3873f1b868e6
abukhleif <moh.noor94@gmail.com>
2018-08-18 14:59:16 +0300

    Corrections in 'LonelyInteger'

--- src/LonelyInteger.java
+++ src/LonelyInteger.java
@@ -1,15 +1,12 @@
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class LonleyInteger {
+public class LonelyInteger {
     public static void main(String[] args) {
-        System.out.println(lonelyinteger(new int[]{1, 2, 3, 6, 5, 6, 5, 2, 1}));
-
-//        int x = 18;
-//        System.out.println(Integer.toBinaryString((5 << 2)));
+        System.out.println(lonelyInteger(new int[]{1, 2, 3, 6, 5, 6, 5, 2, 1}));
     }
 
-    static int lonelyinteger(int[] a) {
+    private static int lonelyInteger(int[] a) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i : a) {
             if (map.containsKey(i))
